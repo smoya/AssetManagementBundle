515b5de7b585e042ca198de1944e5ad25255d10c
Sebastian Grodzicki <sebastian@grodzicki.pl>
2012-09-19 14:44:01 +0200

    Unused variables

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -13,7 +13,7 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('smoya_assetmanagement');
+        $treeBuilder->root('smoya_assetmanagement');
 
         // Here you should define the parameters that are allowed to
         // configure your bundle. See the documentation linked above for
